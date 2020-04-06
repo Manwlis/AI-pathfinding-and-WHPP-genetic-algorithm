fd61e3ed3bde0356e53be25543a97f367a207466
Manolis <33401961+Manwlis@users.noreply.github.com>
2020-04-06 15:59:16 +0300

    3.0
    State.java:
    τα states κρατάνε λίστα με τα παιδιά τους.
    Αλλαγη κονστράκτορα 2 για να γίνεται εκει ο υπολογισμός της νέας θέσης. Επίσης γίνεται εκεί η σύνδεση μεταξύ παιδιών και πατέρα.
    Αλλαγή μεθόδου IsValidMove() ώστε να επιστέφει το κόστος της κίνησης.
    CreateChildren(). Δοκιμάζει όλες τισ πιθανές κινήσεις και φτιάχνει νέες καταστάσεις.
    BFS(). snippet

--- part-A/code/State.java
+++ part-A/code/State.java
@@ -1,9 +1,11 @@
+import java.util.LinkedList;
 import java.util.stream.IntStream;
 
 // 
 public class State 
 {
     private State parent; // goneas
+    private LinkedList<State> children = new LinkedList<>();
 
     private Grid grid; // to grid tou problhmatos. Blepoun to idio oles oi katastaseis alla den to allazoun
     private int  position_idx; // 8esh panw sto grid
@@ -29,33 +31,45 @@ public class State
     }
 
     // create non-root node
-    public State( State parent , int new_position_idx , int move_cost )
+    public State( State parent , int move_idx , int move_cost )
     {
-        this.parent = parent;
-
         this.grid = parent.grid;
 
-        this.position_idx = new_position_idx;
+        // sundesh paidiou kai patera
+        this.parent = parent;
+        parent.children.add( this );
 
-        this.old_positions_idx = parent.old_positions_idx.clone(); // krataei tis palies 8eseis apo to patera
-        this.old_positions_idx[ parent.depth ] = parent.position_idx; // dhlwnei oti exei episkeu8ei ton patera
+        // ipologismos neas 8eshs
+        int [] new_position = new int [2];
+        int [] move = new int [2];
 
+        new_position = IdxToPos( this.position_idx , this.grid.getNumOfColumns() ).clone();
+        move = IdxToPos( move_idx , 10 ).clone();
+
+        new_position[0] = new_position[0] + move[0];
+        new_position[1] = new_position[1] + move[1];
+
+        this.position_idx = PosToIdx( new_position , this.grid.getNumOfColumns() );
+
+        // pairnei apo ton patera ton pinaka me tis prohgoumenes 8eseis. Bazei kai auton mesa.
+        this.old_positions_idx = parent.old_positions_idx.clone();
+        this.old_positions_idx[ parent.depth ] = parent.position_idx;
+
+        // ipologismos neou ba8ous kai kostous
         this.depth = parent.depth + 1;
         this.accumulated_cost = parent.accumulated_cost + move_cost;
     }
 
-    private boolean IsGoalState()
-    {
-        return ( this.grid.getTerminalidx() == this.position_idx );
-    }
+    private boolean IsGoalState() { return ( this.grid.getTerminalidx() == this.position_idx ); }
 
-    // possible moves +01 / +10 / -01 / -10
-    // koitaei an mia kinhsh einai egkurh
-    private boolean IsValidMove( int move_idx )
+
+    /* Elenxei an mia kinhsh einai egkurh. Epistrefei to kostos ths an einai, alliws 0.  */
+    private int IsValidMove( int move_idx )
     {
         int num_columns = this.grid.getNumOfColumns();
         int num_rows = this.grid.getNumOfRows();
 
+        /*---------------------- Upologismos neas 8eshs ----------------------*/
         // metatroph 8eshs kai kinhshs se [i][j] morfh
         int [] new_position = new int [2];
         int [] move = new int [2];
@@ -65,58 +79,104 @@ public class State
         // ipologismos neas 8eshs
         new_position[0] = new_position[0] + move[0];
         new_position[1] = new_position[1] + move[1];
+
+        // metatroph neas 8eshs se index morfh
         int new_position_idx = PosToIdx( new_position , num_columns );
 
+        /*------------------ elenxos ekgurothtas neas 8eshs ------------------*/
         // ektos oriwn
         if ( new_position[0] < 0 || new_position[0] >= num_columns )
-        {
-            return false;
-        }
+            return 0;
         if ( new_position[1] < 0 || new_position[1] >= num_rows )
-        {
-            return false;
-        }
-
+            return 0;
         // lrta* mporei na to xrhsimopoieisei auto????
         // blepei an xtupaei toixo
         if ( this.grid.getCell( new_position[0] , new_position[1] ).isWall() )
-        {
-            return false;
-        }
+            return 0;
         // exei ksanaepiskeu8ei authn th 8esh
         if ( IntStream.of( this.old_positions_idx ).anyMatch(x -> x == new_position_idx ) )
+            return 0;
+
+        return this.grid.getCell( new_position[0] , new_position[1] ).getCost();
+    }
+
+    /* elenxei an oles oi pi8anes einai egkures, kai dhmiourgei kombous gia autes pou einai *
+     * pi8anes kiniseis: panw +01, deksia +10, katw -01, aristera -10                       */
+    private void CreateChildren()
+    {
+        // kinish pros ta panw
+        int cost = this.IsValidMove( 01 );
+        if ( cost != 0 )
         {
-            return false;
+            State child = new State( this , 01 , cost );
         }
-        
-
-
-        return true;
+        // deksia
+        cost = this.IsValidMove( 10 );
+        if ( cost != 0 )
+        {
+            State child = new State( this , 10 , cost );
+        }
+        // katw
+        cost = this.IsValidMove( -01 );
+        if ( cost != 0 )
+        {
+            State child = new State( this , -01 , cost );
+        }
+        // aristera
+        cost = this.IsValidMove( -10 );
+        if ( cost != 0 )
+        {
+            State child = new State( this , -10 , cost );
+        }
     }
 
-    private void CreateChildren(){
 
+    // isws ta pia exoun episkeu8ei xreiazetai mono se auton ton algori8mo. Tote na metafer8ei to [] old_positions_idx edw. Den nomizw na isxuei.
+    // xreiazetai allages gia na katalabainei ta kosth.
+    public State BFS( State node )
+    {
+        // uninitialized state
+        if ( node == null )
+            return null;
+
+        if ( node.IsGoalState() )
+            return node;
+
+        // kataskeuh FIFO ouras. Mpainei h riza.
+        LinkedList<State> frontier = new LinkedList<>();
+        frontier.add( node );
+
+        while ( !frontier.isEmpty() )
+        {
+            State parent = frontier.removeFirst();
+            // create kids
+            parent.CreateChildren();
+
+            // for (children)
+            // {
+            //     if child.IsGoalState()
+            //         return child;
+                
+            //     frontier.add( child );
+            // }
+        }
+        return null;
     }
 
-    public void SearchGoal(){
-
-    }
 
     private void ExtractSolution(){
 
     }
 
+
     // metatrepei mia 8esh apo morfh idx se morfh [i][j]
     private int[] IdxToPos( int idx , int num_columns )
     {
         int [] position = new int[2];
-
         position[0] = idx / num_columns;
         position[1] = idx % num_columns;
-
         return position;
     }
-
     // metatrepei mia 8esh apo morfh [i][j] se morfh idx
     private int PosToIdx( int [] position , int num_columns )
     {
